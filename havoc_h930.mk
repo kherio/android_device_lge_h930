3d09f95921dabb573517998dee266f98e3ea54d5
kherio <jvrsanjose@gmail.com>
2020-02-28 15:55:13 +0100

    Update and rename lineage_h930.mk to havoc_h930.mk

--- havoc_h930.mk
+++ havoc_h930.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/lge/h930/full_h930.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
@@ -14,7 +14,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device identifiers
 
-PRODUCT_NAME := lineage_h930
+PRODUCT_NAME := havoc_h930
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := V30
@@ -25,8 +25,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 9 PKQ1.190414.001 192451212666b release-keys"
+    PRIVATE_BUILD_DESC="joan_global_com-user 8 OPR1.170623.026 181381736b4e9 release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:9/PKQ1.190414.001/192451212666b:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/181381736b4e9:user/release-keys" 
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model ro.product.system.model
